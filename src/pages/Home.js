fdaed1cb60abbdb9dea6ca67a678fd144becd04b
Tejas Nampalli <TSNampalli@gmail.com>
2024-04-03 23:11:46 -0400

    creating page routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,34 @@
-import React, {useState} from "react";
-import NavBar from "../components/NavBar";
-import AlbumForm from "../components/AlbumForm";
-import App from "../components/App";
+// import React, {useState} from "react";
+// import NavBar from "../components/NavBar";
+// import AlbumForm from "../components/AlbumForm";
+// import App from "../components/App";
 
-function Home() {
-    const [albums, setAlbums] = useState([])
+// function Home() {
+//     const [albums, setAlbums] = useState([])
 
 
-// POST REQUEST
+// // POST REQUEST
 
-// function onAddAlbum(newAlbum) {
-//     const updatedAlbums = [...albums, newAlbum]
-//     setAlbums(updatedAlbums)
+// // function onAddAlbum(newAlbum) {
+// //     const updatedAlbums = [...albums, newAlbum]
+// //     setAlbums(updatedAlbums)
+// // }
+
+// return (
+
+// <>
+// <header className="headers">
+//     <h1>CLASSIC ALBUMS App
+//         <span className="logo" role="img">
+//         </span>
+//     </h1>
+//     <NavBar />
+//     </header>
+//     <AlbumForm onAddAlbum={onAddAlbum} />
+//     <App 
+//     albums={albums}
+//     setAlbums={setAlbums}
+//     />
+// </>
+//     )
 // }
-
-return (
-
-<>
-<header className="headers">
-    <h1>CLASSIC ALBUMS App
-        <span className="logo" role="img">
-        </span>
-    </h1>
-    <NavBar />
-    </header>
-    <AlbumForm onAddAlbum={onAddAlbum} />
-    <App 
-    albums={albums}
-    setAlbums={setAlbums}
-    />
-</>
-    )
-}
